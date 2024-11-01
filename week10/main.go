c5037ff86dabb96046335bc1f8593ed94fee4b5e
iso960 <iso960@naver.com>
2024-11-01 15:24:58 +0900

    v2.8 Prime number

--- week10/main.go
+++ week10/main.go
@@ -34,14 +34,14 @@ func main() {
 	} else if n%2 == 0 {
 		isPrime = false
 	} else {
-		j := 2
+		j := 3
 		for j <= int(math.Sqrt(float64(n))) {
 			if n%j == 0 {
 				isPrime = false
 				break // performance up
 			}
 			fmt.Printf("%d ", j) // Check j loop
-			j++
+			j += 2
 		}
 	}
 
