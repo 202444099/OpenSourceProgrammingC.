414b4eda9bdd5b51b8774b291713c2f5a98f7890
iso960 <iso960@naver.com>
2024-11-01 15:17:44 +0900

    v2.6 Prime number

--- week10/main.go
+++ week10/main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	//fmt.Printf("%f\n", math.Sqrt(16.0))
 	fmt.Print("정수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	i, err := in.ReadString('\n')
@@ -29,7 +31,7 @@ func main() {
 		isPrime = false
 	} else {
 		j := 2
-		for j < n {
+		for j <= int(math.Sqrt(float64(n))) {
 			if n%j == 0 {
 				isPrime = false
 				break // performance up
