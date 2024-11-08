132e9095cfc407d63dca55b972fd67a3df9aad34
iso960 <iso960@naver.com>
2024-11-08 15:15:58 +0900

    v3.6 Prime number

--- week11/keyboard/keyboard.go
+++ week11/keyboard/keyboard.go
@@ -1,23 +1,40 @@
-package keybord
+package keyboard
 
 import (
 	"bufio"
-	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func getInteger() int {
+func GetFloat() (float64, error) {
 	in := bufio.NewReader(os.Stdin)
 	a, err := in.ReadString('\n')
+
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 	a = strings.TrimSpace(a)
-	n, _ := strconv.Atoi(a)
+	n, err := strconv.ParseFloat(a, 64)
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
-	return n
+
+	return n, nil
+}
+
+func GetInteger() (int, error) {
+	in := bufio.NewReader(os.Stdin)
+	a, err := in.ReadString('\n')
+
+	if err != nil {
+		return 0, err
+	}
+	a = strings.TrimSpace(a)
+	n, err := strconv.Atoi(a)
+	if err != nil {
+		return 0, err
+	}
+
+	return n, nil
 }
