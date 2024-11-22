d51bd1fa646e98bc791412155894f8431abb2a92
iso960 <iso960@naver.com>
2024-11-22 14:27:47 +0900

    v4.2 Slice

--- week13/main.go
+++ week13/main.go
@@ -5,9 +5,13 @@ import (
 )
 
 func main() {
+	var emptySlice []bool
+	//emptySlice = make([]bool, 5)
+	fmt.Printf("%#v\n", emptySlice) // slice zero value(nil)
+
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
 	gpa_slice := gpa[1:4] // 4.1, 4.5, 3.9
-	// gpa_slicec[1] = 2.71
+	//gpa_slicec[1] = 2.71
 	gpa[2] = 2.71
 	gpa_slice = append(gpa_slice, 4.3, 5.5)
 	fmt.Println(len(gpa_slice), gpa_slice, gpa)
