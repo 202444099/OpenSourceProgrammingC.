b140d8e7dd1c3193fb3d1844023ef6b706e8e7c1
iso960 <iso960@naver.com>
2024-11-29 14:51:21 +0900

    v4.8 Struct

--- week14/main.go
+++ week14/main.go
@@ -2,26 +2,22 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
 	student1.id = 202444099
 	student1.name = "박소이"
 	student1.gpa = 4.5
-
 	fmt.Println(student1.gpa)
 
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student2 student
 	student2.id = 202444098
 	student2.name = "김지윤"
 	student2.gpa = 4.5
-
 	fmt.Println(student2.id)
 }
